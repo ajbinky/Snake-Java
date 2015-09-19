4901317c4bc93328ced00399a0ce2e9c1fcee10b
ajbinky <b1nkyb0y@hotmail.com>
2015-09-19 18:38:46 -0500

    GameBoard.getBoardSize()
    use this to find the max value in the X and Y directions.

--- GameBoard.java
+++ GameBoard.java
@@ -1,12 +1,14 @@
 package ajbinky;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class GameBoard extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private final int pxlSize = 32;
-	//min value of 8 - idk max yet
+	//min value of 16 - idk max yet
 	private int boardMod = 16;
 	private int boardSize = pxlSize * boardMod;
 	
@@ -15,12 +17,18 @@ public class GameBoard extends JFrame {
 	
 	public GameBoard() {
 		
-		add(new Pxl());
+		add(new Pxl(256,256));
 		
 		setTitle("Snake.apk");
 		setSize(boardSize, boardSize);
+		getContentPane().setBackground(Color.BLACK);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
 	}
+	
+	public int getBoardSize() {
+		return boardMod;
+	}
+	
 }
--- Pxl.java
+++ Pxl.java
@@ -10,13 +10,20 @@ import javax.swing.JPanel;
 public class Pxl extends JPanel {
 
 	private static final long serialVersionUID = -8134472978132598224L;
-
+	private int x;
+	private int y;
+	
+	public Pxl(int inx, int iny) {
+		x = inx;
+		y = iny;
+	}
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setPaint(Color.BLACK);
-		g2.fill(new Rectangle2D.Double(10,10,32,32));
+		g2.fill(new Rectangle2D.Double(x,y,32,32));
 	}
 	
 }
